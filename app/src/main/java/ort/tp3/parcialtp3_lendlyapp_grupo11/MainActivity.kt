cc265cfbda326a8aca0da6a92af8298ac108acba
estebanlizaso <estebanlizaso@gmail.com>
2026-05-27 02:04:39 -0300

    fix: se modificaron las imágenes por .png

--- app/src/main/java/ort/tp3/parcialtp3_lendlyapp_grupo11/MainActivity.kt
+++ app/src/main/java/ort/tp3/parcialtp3_lendlyapp_grupo11/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import ort.tp3.parcialtp3_lendlyapp_grupo11.ui.theme.ParcialTP3_LendlyApp_Grupo11Theme
 
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ParcialTP3_LendlyApp_Grupo11Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -32,6 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo),
+        contentDescription = "Logo",
+        modifier = Modifier.fillMaxSize()
+    )
     Text(
         text = "Hello $name!",
         modifier = modifier
--- app/src/main/java/ort/tp3/parcialtp3_lendlyapp_grupo11/ui/theme/Color.kt
+++ app/src/main/java/ort/tp3/parcialtp3_lendlyapp_grupo11/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package ort.tp3.parcialtp3_lendlyapp_grupo11.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import java.util.stream.Gatherer
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Colores de Splash Screen y Onboarding
+val Green = Color(0xFF7BF179)
+
+val GreenDark = Color(0xFF002203)
+
+val GreenLight = Color(0xFFEADDFF29)
+
+
+//Colores de Home
+val GreenLight2 = Color(0xFFB1D18A)
+
+val GreenDark2 = Color(0xFFE5F5EA)
+
+val GreenLight3 = Color(0xFFFCF8F8)
