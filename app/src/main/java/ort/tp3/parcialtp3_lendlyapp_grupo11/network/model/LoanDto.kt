c3af06397178fae5a7041afa288b7526b77154f7
fedev23 <fedevillanueva23@gmail.com>
2026-05-30 18:41:15 -0300

    add network layer for mock api

---
+++ app/src/main/java/ort/tp3/parcialtp3_lendlyapp_grupo11/network/model/LoanDto.kt
@@ -0,0 +1,32 @@
+package ort.tp3.parcialtp3_lendlyapp_grupo11.network.model
+
+data class LoansResponse(
+    val success: Boolean,
+    val loans: List<LoanDto>,
+    val summary: LoansSummaryDto
+)
+
+data class LoanDto(
+    val id: String,
+    val lender: String,
+    val lenderLogo: String,
+    val amount: Double,
+    val amountDue: Double,
+    val installmentAmount: Double,
+    val installmentPlan: String,
+    val interestRate: Double,
+    val purpose: String,
+    val status: String,
+    val nextPaymentDate: String?,
+    val nextPaymentLabel: String?,
+    val startDate: String,
+    val endDate: String,
+    val paidInstallments: Int,
+    val totalInstallments: Int
+)
+
+data class LoansSummaryDto(
+    val totalActive: Int,
+    val totalPaid: Int,
+    val totalAmountDue: Double
+)
